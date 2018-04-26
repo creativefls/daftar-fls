d23ebcb7a67e6c1e1ff007a838abb686abd9c1c3
wkwksama <mailpakgasing@gmail.com>
2018-04-26 16:13:10 +0700

    style finish button

--- components/daftar/WizardActions.vue
+++ components/daftar/WizardActions.vue
@@ -4,8 +4,18 @@
       <v-btn v-if="step > 1" @click.stop="backStep()" round block color="primary" outline>Kembali</v-btn>
     </v-flex>
     <v-flex class="px-1">
-      <v-btn v-if="step < maxStep" @click.stop="nextStep()" round block color="primary" depressed>Lanjut</v-btn>
-      <v-btn v-else @click.stop="finishStep()" round color="primary" depressed>Kirim <v-icon right dark>cloud_upload</v-icon></v-btn>
+      <v-btn v-if="step < maxStep"
+        @click.stop="nextStep()"
+        round
+        block
+        color="primary"
+        depressed>Lanjut</v-btn>
+      <v-btn v-else
+        @click.stop="finishStep()"
+        round
+        block
+        color="primary"
+        depressed>Kirim <v-icon right dark>cloud_upload</v-icon></v-btn>
     </v-flex>
   </v-layout>
 </template>
