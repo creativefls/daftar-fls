d65f51fcd9cf6ad0000ed1a4e186818c9cfdaf13
wkwksama <mailpakgasing@gmail.com>
2018-04-26 16:14:11 +0700

    essay pertanyaan motivasi dan alasan mengapa milih room

--- components/daftar/step-6.vue
+++ components/daftar/step-6.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
-    step6
+    <div class="subheading primary__dark--text">Apa motivasi kamu mengikuti FLS 2018?</div>
+    <v-text-field
+      v-model="model.essayMotivationJoin"
+      data-vv-as="Pertanyaan"
+      :error-messages="errors.collect('motivationJoin')"
+      multi-line
+      counter="300"
+      v-validate="'required'"
+      data-vv-name="motivationJoin"
+      class="pa-0"
+    ></v-text-field>
+
+    <div class="subheading primary__dark--text mt-4">Mengapa kamu memilih room tersebut?</div>
+    <v-text-field
+      v-model="model.essayRoomSelected"
+      data-vv-as="Pertanyaan"
+      :error-messages="errors.collect('roomSelected')"
+      multi-line
+      counter="300"
+      v-validate="'required'"
+      data-vv-name="roomSelected"
+      class="pa-0"
+    ></v-text-field>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      model: {
+        essayMotivationJoin: '',
+        essayRoomSelected: ''
+      }
+    }
+  },
   methods: {
     validate () {
       return new Promise((resolve, reject) => {
