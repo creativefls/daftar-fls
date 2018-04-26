3e025833ebbdf7cfaeafe5a2822aaf5bd061cd33
wkwksama <mailpakgasing@gmail.com>
2018-04-26 14:50:26 +0700

    fix layout button wizard actions

--- components/daftar/WizardActions.vue
+++ components/daftar/WizardActions.vue
@@ -1,9 +1,13 @@
 <template>
-  <v-card-actions>
-    <v-btn v-if="step > 1" @click.stop="backStep()" round color="primary" outline>Kembali</v-btn>
-    <v-btn v-if="step < maxStep" @click.stop="nextStep()" round color="primary" depressed>Lanjut</v-btn>
-    <v-btn v-else @click.stop="finishStep()" round color="primary" depressed>Kirim <v-icon right dark>cloud_upload</v-icon></v-btn>
-  </v-card-actions>
+  <v-layout>
+    <v-flex class="px-1">
+      <v-btn v-if="step > 1" @click.stop="backStep()" round block color="primary" outline>Kembali</v-btn>
+    </v-flex>
+    <v-flex class="px-1">
+      <v-btn v-if="step < maxStep" @click.stop="nextStep()" round block color="primary" depressed>Lanjut</v-btn>
+      <v-btn v-else @click.stop="finishStep()" round color="primary" depressed>Kirim <v-icon right dark>cloud_upload</v-icon></v-btn>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
