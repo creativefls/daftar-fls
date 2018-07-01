2a3928d6f7009a9c39360f7d863361340c00ff70
wkwksama <mailpakgasing@gmail.com>
2018-07-01 09:54:28 +0700

    link poster auto download

---
+++ pages/temp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>asd</div>
+</template>
+
+<script>
+import swal from 'sweetalert2'
+
+export default {
+  data () {
+    return {
+      formModel: {
+        nickName: 'gasing',
+        email: 'asdfa@adf.com'
+      }
+    }
+  },
+  mounted()  {
+    let self = this
+    swal({
+      title: 'Terima kasih, ' + self.formModel.nickName,
+      html: 'Kamu telah terdaftar dengan email <b>' + self.formModel.email + '</b>. <a href="/v.png" download>lalala</a> Ingat selalu email ini, karena pengumuman hasil seleksi akan menggunakan email tersebut. Untuk info lebih lanjut pantau sosial media FLS 2018',
+      type: 'success',
+      allowOutsideClick: false,
+      confirmButtonText: 'OK',
+    })
+  }
+}
+</script>
